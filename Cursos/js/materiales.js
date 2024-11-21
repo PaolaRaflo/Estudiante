3b3ee44ecc9148d14c5812b84d8c859f6c0a6779
Paou <paolaraflo914@gmail.com>
2024-11-20 22:24:56 -0500

    MODULO MATERIALES TERMINADO OK

--- Cursos/js/materiales.js
+++
@@ -1,8 +0,0 @@
-
-
-$(document).ready(function () {
-    $('.btn-ingresar').on('click', function () {
-        const curso = $(this).data('curso');
-        window.location.href = `curso.html?curso=${encodeURIComponent(curso)}`;
-    });
-});
